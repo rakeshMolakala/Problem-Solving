8d1365e2c5619fc1c30370831a77529a30109707
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-05-24 22:36:01 -0400

    Time: 55 ms (37.68%), Space: 92.9 MB (27.72%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -6,24 +6,16 @@ class Solution {
             Set<Integer> set1 = new HashSet<>();
             Set<Integer> set2 = new HashSet<>();
             for(int j=0;j<n;j++) {
+                //row wise search
                 set1.add(matrix[i][j]);
+                
+                //column wise search
                 set2.add(matrix[j][i]);
             }
             if(set1.size()!=n || set2.size()!=n){
                 return false;
             }
         }
-        
-        // //check every row
-        // for(int i = 0;i<n;i++){
-        //     Set<Integer> set = new HashSet<>();
-        //     for(int j=0;j<n;j++) {
-        //         set.add(matrix[j][i]);
-        //     }
-        //     if(set.size()!=n){
-        //         return false;
-        //     }
-        // }
         
         return true;
         
