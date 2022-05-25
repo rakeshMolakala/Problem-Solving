187cf139da24af73a879488d3017f80148db5ce7
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-05-24 22:35:29 -0400

    Time: 46 ms (51.18%), Space: 97.1 MB (9.79%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -3,25 +3,27 @@ class Solution {
         //check every row
         int n=matrix.length;
         for(int i = 0;i<n;i++){
-            Set<Integer> set = new HashSet<>();
+            Set<Integer> set1 = new HashSet<>();
+            Set<Integer> set2 = new HashSet<>();
             for(int j=0;j<n;j++) {
-                set.add(matrix[i][j]);
+                set1.add(matrix[i][j]);
+                set2.add(matrix[j][i]);
             }
-            if(set.size()!=n){
+            if(set1.size()!=n || set2.size()!=n){
                 return false;
             }
         }
         
-        //check every row
-        for(int i = 0;i<n;i++){
-            Set<Integer> set = new HashSet<>();
-            for(int j=0;j<n;j++) {
-                set.add(matrix[j][i]);
-            }
-            if(set.size()!=n){
-                return false;
-            }
-        }
+        // //check every row
+        // for(int i = 0;i<n;i++){
+        //     Set<Integer> set = new HashSet<>();
+        //     for(int j=0;j<n;j++) {
+        //         set.add(matrix[j][i]);
+        //     }
+        //     if(set.size()!=n){
+        //         return false;
+        //     }
+        // }
         
         return true;
         
