9866c865c1fc19b69f2ef8bb3de0330312d41fba
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-06-02 19:07:03 -0400

    Time: 343 ms (17.06%), Space: 213.9 MB (31.77%) - LeetHub

--- 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.java
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.java
@@ -47,51 +47,48 @@ class Solution {
                     q.add(currCon);
                     dist[currCon] = currDist+1;
                     
-                    // Calculating time for every node
+//                     // Calculating time for every node
                     
-                    int transTime = dist[currCon]*2;
-                    if(transTime<=patience[currCon]){
-                        time[currCon] = transTime;
-                    }
-                    else{
-                        int packets = transTime/patience[currCon];
-                        if(transTime%patience[currCon]==0){
-                            packets = packets - 1;
-                        }
-                        int lastPacketTime = packets*patience[currCon];
-                        time[currCon] = lastPacketTime + transTime;
-                    }
-                    if(time[currCon]>res){
-                        res = time[currCon];
-                    }
+//                     int transTime = dist[currCon]*2;
+//                     if(transTime<=patience[currCon]){
+//                         time[currCon] = transTime;
+//                     }
+//                     else{
+//                         int packets = transTime/patience[currCon];
+//                         if(transTime%patience[currCon]==0){
+//                             packets = packets - 1;
+//                         }
+//                         int lastPacketTime = packets*patience[currCon];
+//                         time[currCon] = lastPacketTime + transTime;
+//                     }
+//                     if(time[currCon]>res){
+//                         res = time[currCon];
+//                     }
                     
                     
                 }
             }
         }
         
-        // time[0] = 0;
-        // for(int i =0;i<n;i++){
-        //     int transTime = dist[i]*2;
-        //     if(transTime<=patience[i]){
-        //         time[i] = transTime;
-        //     }
-        //     else{
-        //         int packets = transTime/patience[i];
-        //         if(transTime%patience[i]==0){
-        //             packets = packets - 1;
-        //         }
-        //         int lastPacketTime = packets*patience[i];
-        //         time[i] = lastPacketTime + transTime;
-        //     }    
-        // }
-        
-        // int res = 0;
-        // for(int i = 0;i<n;i++){
-        //     if(time[i]>res){
-        //         res = time[i];
-        //     }
-        // }
+        time[0] = 0;
+        for(int i =0;i<n;i++){
+            int transTime = dist[i]*2;
+            if(transTime<=patience[i]){
+                time[i] = transTime;
+            }
+            else{
+                int packets = transTime/patience[i];
+                if(transTime%patience[i]==0){
+                    packets = packets - 1;
+                }
+                int lastPacketTime = packets*patience[i];
+                time[i] = lastPacketTime + transTime;
+            }
+            if(time[i]>res){
+                res = time[i];
+            }
+            
+        }
         
         return res+1;
         
