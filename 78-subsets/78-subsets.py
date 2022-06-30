b06f95255241e2e114b1952044542057dfbde910
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-06-30 07:46:33 -0400

    Time: 556 ms (5.20%), Space: 15.2 MB (33.88%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        def backtrack(i,arr):
+            if(i==len(nums)):
+                res.append(arr[:])
+                print(res)
+                return
+            arr.append(nums[i])
+            backtrack(i+1,arr)
+            arr.pop()
+            backtrack(i+1,arr)
+            
+        res=[]
+        backtrack(0,[])
+        return res
+            
+            
+        
