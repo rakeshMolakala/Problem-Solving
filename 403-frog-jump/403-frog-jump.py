da7a442e19d7c04f5e6752317e3691d9a89a81f3
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-06-30 22:12:47 -0400

    Time: 5095 ms (5.52%), Space: 23 MB (39.53%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        
+        def recur(i,prev):
+            if((i,prev) in cache):
+                return cache[(i,prev)]
+            
+            res = False
+            if(i==len(stones)-1):
+                cache[(i,prev)] = True
+                return True
+
+            poss = [prev-1,prev,prev+1]
+            j=i+1
+            
+            
+            while(j<len(stones)):
+                
+                if((stones[j]-stones[i]) in poss) and  (recur(j,stones[j]-stones[i])):
+                    return True
+                    break
+                j=j+1
+                
+            cache[(i,prev)] = res
+            return res
+                
+        if(len(stones)>1 and stones[1]-stones[0]>1):
+            return False
+        
+        cache = dict()
+        return recur(1,1)
+        
