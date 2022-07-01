e936ef4fbddf8a1814f0c4f46738177838cbc418
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-06-30 22:34:38 -0400

    Time: 518 ms (31.03%), Space: 23 MB (40.50%) - LeetHub

--- 403-frog-jump/403-frog-jump.py
+++ 403-frog-jump/403-frog-jump.py
@@ -12,7 +12,7 @@ class Solution:
             poss = [prev-1,prev,prev+1]
             j=i+1
             
-            while(j<len(stones)):
+            while(j<len(stones) and stones[j]-stones[i]<=prev+1):
                 
                 if((stones[j]-stones[i]) in poss) and  (recur(j,stones[j]-stones[i])):
                     return True
