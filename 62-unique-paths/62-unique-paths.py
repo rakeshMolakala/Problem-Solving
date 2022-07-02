0df84bf8f25fea5eae9d5473689e776eb8b5af99
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-02 03:41:40 -0400

    Time: 69 ms (8.56%), Space: 14.2 MB (7.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,15 +1,65 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[0]*n for i in range(m)]
-        dp[0][0] = 1
-        for i in range(1,m):
-            dp[i][0] = 1
-        for j in range(1,n):
-            dp[0][j] = 1
-            
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-        return dp[m-1][n-1]
+        
+        def recur(row,col):
+            if((row,col) in cache):
+                return cache[(row,col)]
+            
+            if(row>=m or col>=n):
+                return 0
+            if(row==m-1 and col==n-1):
+                return 1
+            
+            p1 = recur(row+1,col)
+            p2 = recur(row,col+1)
+            res = p1+p2
+            cache[(row,col)] = res
+            return res
+        
+        cache = dict()
+        return recur(0,0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dp=[[0]*n for i in range(m)]
+#         dp[0][0] = 1
+#         for i in range(1,m):
+#             dp[i][0] = 1
+#         for j in range(1,n):
+#             dp[0][j] = 1
+            
+#         for i in range(1,m):
+#             for j in range(1,n):
+#                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+#         return dp[m-1][n-1]
         
