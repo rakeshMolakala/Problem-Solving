caf7afd0f417b4956cb20dc021804fdc73613aae
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-02 05:28:02 -0400

    Time: 158 ms (46.88%), Space: 16.2 MB (18.03%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,25 +1,53 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[None]*n for _ in range(m)]
         
-        def recur(row,col):
-            if(row==m-1 and col==n-1):
-                return grid[row][col]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if(i==0 and j==0):
+                    dp[i][j] = grid[0][0]
+                    continue
+                up = grid[i][j]
+                left = grid[i][j]
+                if(i>0):
+                    up = up + dp[i-1][j]
+                else:
+                    up = up + dp[i][j-1]
+                if(j>0):
+                    left = left + dp[i][j-1]
+                else:
+                    left = left + dp[i-1][j]
+                dp[i][j] = min(up,left)
+        
+        print(dp)
+        return dp[m-1][n-1]
+                
+        
+        
+#         Top down approach
+
+#         def recur(row,col):
+#             if(row==m-1 and col==n-1):
+#                 return grid[row][col]
             
-            if((row,col) in cache):
-                return cache[(row,col)]
+#             if((row,col) in cache):
+#                 return cache[(row,col)]
+             
+#             p1=math.inf
+#             p2=math.inf
+#             if(row+1<m): 
+#                 p1 = grid[row][col] + recur(row+1,col)
+#             if(col+1<n):
+#                 p2 = grid[row][col] + recur(row,col+1)
             
-            p1=math.inf
-            p2=math.inf
-            if(row+1<m): 
-                p1 = grid[row][col] + recur(row+1,col)
-            if(col+1<n):
-                p2 = grid[row][col] + recur(row,col+1)
+#             res = min(p1,p2)
+#             cache[(row,col)] = res
+#             return res
             
-            res = min(p1,p2)
-            cache[(row,col)] = res
-            return res
-            
-        cache = dict()
-        m=len(grid)
-        n=len(grid[0])
-        return recur(0,0)
+#         cache = dict()
+#         m=len(grid)
+#         n=len(grid[0])
+#         return recur(0,0)
