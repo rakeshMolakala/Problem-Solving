94d2ca5464318fea3d29ce3b2d418963499f3266
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-03 12:10:30 -0400

    Time: 1175 ms (36.79%), Space: 44.7 MB (32.84%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.py
+++ 741-cherry-pickup/741-cherry-pickup.py
@@ -17,16 +17,8 @@ class Solution:
             if (j1!=j2):
                 res = res + grid[i2][j2]
             
-            possible_moves = [[1,0],[0,1]]
-            maxi = -math.inf
-            for x in possible_moves:
-                p1 = [i1+x[0],j1+x[1]]
-                for y in possible_moves:
-                    p2 = [i2+y[0],j2+y[1]]
-                    
-                    maxi = max(maxi,recur(p1[0],p1[1],p2[1]))
-                      
-            res = res + maxi
+            res = res + max(recur(i1+1,j1,j2),recur(i1+1,j1,j2+1),recur(i1,j1+1,j2),recur(i1,j1+1,j2+1))
+            
             cache[(i1,j1,j2)] = res
             return res
 
