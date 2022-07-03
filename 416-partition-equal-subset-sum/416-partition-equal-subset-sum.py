aeff9956fd998e468ec94013420cb49264aac660
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-03 18:41:01 -0400

    Time: 5258 ms (9.11%), Space: 54.7 MB (13.30%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -59,22 +59,21 @@ class Solution:
         
         
         
-#         s = sum(nums)
-#         n= len(nums)
-#         if(s%2!=0):
-#             return False;
-#         s=s//2
-#         dp=[[False]*(s+1) for i in range(n+1)]
-#         dp[0][0]=True
+        s = sum(nums)
+        n= len(nums)
+        if(s%2!=0):
+            return False;
+        s=s//2
+        dp=[[False]*(s+1) for i in range(n+1)]
+        dp[0][0]=True
         
-#         for i in range(n+1):
-#             dp[i][0]=True
-#         for i in range(1,n+1):
-#             for j in range(1,s+1):
-#                 if(nums[i-1]<=j):
-#                     dp[i][j] = (dp[i-1][j-nums[i-1]]) or (dp[i-1][j])
-#                 else:
-#                     dp[i][j] = dp[i-1][j]
-#         # print(dp)
-#         return dp[n][s]
+        for i in range(n+1):
+            dp[i][0]=True
+        for i in range(1,n+1):
+            for j in range(1,s+1):
+                if(nums[i-1]<=j):
+                    dp[i][j] = (dp[i-1][j-nums[i-1]]) or (dp[i-1][j])
+                # else:
+                #     dp[i][j] = dp[i-1][j]
+        return dp[n][s]
         
