0051ca82a51f5e711ac94784cd9e528e4e04aad2
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-03 22:53:06 -0400

    Time: 4164 ms (17.01%), Space: 29.4 MB (40.05%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -102,16 +102,16 @@ class Solution:
         if(s%2!=0):
             return False;
         s=s//2
-        dp = [[False]*(s+1) for _ in range(n)]
-        for i in range(n):
+        dp = [[False]*(s+1) for _ in range(n+1)]
+        for i in range(n+1):
             dp[i][0] = True
         
-        for i in range(1,n):
+        for i in range(1,n+1):
             for j in range(1,s+1):
                 notTake = dp[i-1][j]
                 take = False
-                if(nums[i]<=j):
-                    take = dp[i-1][j-nums[i]]
+                if(nums[i-1]<=j):
+                    take = dp[i-1][j-nums[i-1]]
                 dp[i][j] = take or notTake
-        return dp[n-1][s]
+        return dp[n][s]
         
