0d774754fa7b9d6412b53a88afbb059cb51758c5
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-04 18:18:28 -0400

    Time: 3834 ms (6.11%), Space: 399.5 MB (5.45%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,51 +1,88 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #Bottom Up
-        dp = [math.inf]*(amount+1)
-        dp[0] = 0
         
-        for a in range(1,amount+1):
-            for c in coins:
-                remain = a - c
-                if(remain>=0):
-                    dp[a] = min(dp[a],1+dp[remain])
-        
-        if(dp[amount]!=math.inf):
-            return dp[amount]
-        return -1
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        #Top Down approach
-        cache = dict()
-        cache[0] = 0
-        
-        def recur(remain):
-            if(remain<0):
-                return math.inf
-            if(remain==0):
-                return 0
-            if(remain in cache):
-                return cache[remain]
-            for x in coins:
-                if(remain in cache):
-                    cache[remain] = min(cache[remain],recur(remain-x)+1)
+        def recur(i,target):
+            if(i==0):
+                if(target%coins[i]==0):
+                    return target//coins[i]
                 else:
-                    cache[remain] = recur(remain-x)+1
-            return cache[remain]
-
-        recur(amount)
-        if(cache[amount]==math.inf):
+                    return math.inf
+                
+            if((i,target) in cache):
+                return cache[(i,target)]
+            
+            notTake = recur(i-1,target)
+            take = math.inf
+            if(target>=coins[i]):
+                take = 1+recur(i,target-coins[i])
+            
+            res = min(take,notTake)
+            cache[(i,target)] = res
+            return res
+        
+        cache = dict()
+        res = recur(len(coins)-1,amount)
+        if(res==math.inf):
             return -1
-        return cache[amount] 
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         #Bottom Up
+#         dp = [math.inf]*(amount+1)
+#         dp[0] = 0
+        
+#         for a in range(1,amount+1):
+#             for c in coins:
+#                 remain = a - c
+#                 if(remain>=0):
+#                     dp[a] = min(dp[a],1+dp[remain])
+        
+#         if(dp[amount]!=math.inf):
+#             return dp[amount]
+#         return -1
+        
+        
+    
+#         #Top Down approach
+#         cache = dict()
+#         cache[0] = 0
+        
+#         def recur(remain):
+#             if(remain<0):
+#                 return math.inf
+#             if(remain==0):
+#                 return 0
+#             if(remain in cache):
+#                 return cache[remain]
+#             for x in coins:
+#                 if(remain in cache):
+#                     cache[remain] = min(cache[remain],recur(remain-x)+1)
+#                 else:
+#                     cache[remain] = recur(remain-x)+1
+#             return cache[remain]
+
+#         recur(amount)
+#         if(cache[amount]==math.inf):
+#             return -1
+#         return cache[amount] 
                 
                 
                 
