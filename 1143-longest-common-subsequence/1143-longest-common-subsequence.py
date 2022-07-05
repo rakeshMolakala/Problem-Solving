8202e360e8b42d9831e820e914ce4aee0319235a
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-05 11:39:04 -0400

    Time: 1930 ms (7.09%), Space: 138.8 MB (18.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,39 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
+#         def recur(i1,i2):
+#             if(i1<0 or i2<0):
+#                 return ""
+#             if((i1,i2) in cache):
+#                 return cache[(i1,i2)]
+#             if(text1[i1]==text2[i2]):
+#                 return recur(i1-1,i2-1)+text1[i1]
+#             p1 = recur(i1,i2-1)
+#             p2 = recur(i1-1,i2)
+#             res = ""
+            
+#             if(len(p1)>p2):
+                
+#             cache[(i1,i2)] = max(p1,p2)
+#             return max(p1,p2)
+        
+#         cache = dict()
+#         return recur(len(text1)-1,len(text2)-1)
+        
+        
         # Bottom up with space optimization
-        m= len(text1)
-        n = len(text2)
-        prev = [0]*(n+1)
-        curr = [0]*(n+1)
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if(text1[i-1]==text2[j-1]):
-                    curr[j] = 1 + prev[j-1]
-                else:
-                    curr[j] = max(prev[j],curr[j-1])
-            prev = curr.copy()
-        return prev[n]
+        # m= len(text1)
+        # n = len(text2)
+        # prev = [0]*(n+1)
+        # curr = [0]*(n+1)
+        # for i in range(1,m+1):
+        #     for j in range(1,n+1):
+        #         if(text1[i-1]==text2[j-1]):
+        #             curr[j] = 1 + prev[j-1]
+        #         else:
+        #             curr[j] = max(prev[j],curr[j-1])
+        #     prev = curr.copy()
+        # return prev[n]
         
         
         # Bottom up 
@@ -30,19 +50,20 @@ class Solution:
            
     
         # Top down approach
-#         def recur(i1,i2):
-#             if(i1<0 or i2<0):
-#                 return 0
-#             if((i1,i2) in cache):
-#                 return cache[(i1,i2)]
-#             if(text1[i1]==text2[i2]):
-#                 return 1+recur(i1-1,i2-1)
-#             p1 = recur(i1,i2-1)
-#             p2 = recur(i1-1,i2)
-#             cache[(i1,i2)] = max(p1,p2)
-#             return max(p1,p2)
+        def recur(i1,i2,):
+            if(i1<0 or i2<0):
+                return 0
+            if((i1,i2) in cache):
+                return cache[(i1,i2)]
+            if(text1[i1]==text2[i2]):
+                cache[(i1,i2)] = 1+recur(i1-1,i2-1)
+                return cache[(i1,i2)]
+            p1 = recur(i1,i2-1)
+            p2 = recur(i1-1,i2)
+            cache[(i1,i2)] = max(p1,p2)
+            return max(p1,p2)
         
-#         cache = dict()
-        # return recur(len(text1)-1,len(text2)-1)
+        cache = dict()
+        return recur(len(text1)-1,len(text2)-1)
     
     
