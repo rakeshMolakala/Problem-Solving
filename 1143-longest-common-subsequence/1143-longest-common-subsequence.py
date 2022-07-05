baf2c52f3bdea0c27fd0841432892676291f41fa
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-05 11:17:29 -0400

    Time: 617 ms (55.85%), Space: 13.9 MB (97.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,22 +1,34 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
+        # Bottom up with space optimization
         m= len(text1)
         n = len(text2)
-        dp = [[0]*(n+1) for _ in range(m+1)]
+        prev = [0]*(n+1)
+        curr = [0]*(n+1)
         for i in range(1,m+1):
             for j in range(1,n+1):
-                
                 if(text1[i-1]==text2[j-1]):
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    curr[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-        return dp[m][n]
-            
+                    curr[j] = max(prev[j],curr[j-1])
+            prev = curr.copy()
+        return prev[n]
         
         
-        
-        
+        # Bottom up 
+#         m= len(text1)
+#         n = len(text2)
+#         dp = [[0]*(n+1) for _ in range(m+1)]
+#         for i in range(1,m+1):
+#             for j in range(1,n+1):
+#                 if(text1[i-1]==text2[j-1]):
+#                     dp[i][j] = 1 + dp[i-1][j-1]
+#                 else:
+#                     dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+#         return dp[m][n]
+           
+    
         # Top down approach
 #         def recur(i1,i2):
 #             if(i1<0 or i2<0):
