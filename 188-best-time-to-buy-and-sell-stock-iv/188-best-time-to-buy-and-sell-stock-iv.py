0dbdb43c2b948c47de3b11749d6fac043d6c21c7
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-07 22:44:39 -0400

    Time: 202 ms (50.29%), Space: 13.9 MB (79.13%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -41,34 +41,63 @@ class Solution:
     
     
         # Bottom up
+#         n = len(prices)
+#         if(n==0):
+#             return 0
+#         dp = [[[0 for _ in range(l+1)] for _ in range(2)] for _ in range(n)]
+        
+#         for j in range(2):
+#             for k in range(l+1):
+#                 # buy
+#                 if j==1:
+#                     dp[n-1][j][k] = 0
+#                 # sell
+#                 else:
+#                     dp[n-1][j][k] = prices[n-1]
+                    
+#         for i in range(n):
+#             for j in range(2):
+#                 dp[i][j][l] = 0
+                
+#         for i in range(n-2,-1,-1):
+#             for j in range(2):
+#                 for k in range(l-1,-1,-1):
+#                     if(j==1):
+#                         p1 = -prices[i] + dp[i+1][0][k]
+#                         p2 = dp[i+1][1][k]
+#                         dp[i][j][k] = max(p1,p2)
+#                     else:
+#                         p1 = prices[i] + dp[i+1][1][k+1]
+#                         p2 = dp[i+1][0][k]
+#                         dp[i][j][k] = max(p1,p2)
+                        
+#         return dp[0][1][0]       
+
+        # Space optimized
         n = len(prices)
         if(n==0):
             return 0
-        dp = [[[0 for _ in range(l+1)] for _ in range(2)] for _ in range(n)]
-        
+        nextt = [[0]*(l+1) for _ in range(2)]
         for j in range(2):
             for k in range(l+1):
-                # buy
-                if j==1:
-                    dp[n-1][j][k] = 0
-                # sell
+                if(j==1):
+                    nextt[j][k]=0
                 else:
-                    dp[n-1][j][k] = prices[n-1]
-                    
-        for i in range(n):
-            for j in range(2):
-                dp[i][j][l] = 0
-                
+                    nextt[j][k]=prices[n-1]
+        for j in range(2):
+            nextt[j][l] = 0
+        curr = [[0]*(l+1) for _ in range(2)]
+
         for i in range(n-2,-1,-1):
             for j in range(2):
                 for k in range(l-1,-1,-1):
                     if(j==1):
-                        p1 = -prices[i] + dp[i+1][0][k]
-                        p2 = dp[i+1][1][k]
-                        dp[i][j][k] = max(p1,p2)
+                        p1 = -prices[i] + nextt[0][k]
+                        p2 = nextt[1][k]
+                        curr[j][k] = max(p1,p2)
                     else:
-                        p1 = prices[i] + dp[i+1][1][k+1]
-                        p2 = dp[i+1][0][k]
-                        dp[i][j][k] = max(p1,p2)
-                        
-        return dp[0][1][0]        
+                        p1 = prices[i] + nextt[1][k+1]
+                        p2 = nextt[0][k]
+                        curr[j][k] = max(p1,p2)
+            nextt = curr.copy()  
+        return nextt[1][0]
