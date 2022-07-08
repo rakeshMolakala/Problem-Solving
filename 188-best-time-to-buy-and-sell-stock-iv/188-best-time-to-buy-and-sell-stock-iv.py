b2f46f4780454a5dc1210fc29c15d9f0cb810ab1
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-07 22:41:02 -0400

    Time: 153 ms (70.28%), Space: 17.2 MB (49.78%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,40 +1,74 @@
 class Solution:
-    def maxProfit(self, k: int, prices: List[int]) -> int:
+    def maxProfit(self, l: int, prices: List[int]) -> int:
         # Top Down
-        n = len(prices)
-        def recur(i,buy,number):
-            # Base cases
-            if(number==k):
-                return 0
-            if(i==n-1):
-                if(buy):
-                    return 0
-                else:
-                    return prices[i]
+#         n = len(prices)
+#         def recur(i,buy,number):
+#             # Base cases
+#             if(number==k):
+#                 return 0
+#             if(i==n-1):
+#                 if(buy):
+#                     return 0
+#                 else:
+#                     return prices[i]
             
             
-            # using Cached result
-            if((i,buy,number) in cache):
-                return cache[(i,buy,number)]
+#             # using Cached result
+#             if((i,buy,number) in cache):
+#                 return cache[(i,buy,number)]
             
-            res = 0
-            if(buy):
-                # will buy
-                p1 = -prices[i] + recur(i+1,False,number)
-                # not buy
-                p2 = recur(i+1,True,number)
-                res = max(p1,p2)
-            else:
-                # will sell
-                p1 = prices[i] + recur(i+1,True,number+1)
-                # not sell
-                p2 = recur(i+1,False,number)
-                res = max(p1,p2)
+#             res = 0
+#             if(buy):
+#                 # will buy
+#                 p1 = -prices[i] + recur(i+1,False,number)
+#                 # not buy
+#                 p2 = recur(i+1,True,number)
+#                 res = max(p1,p2)
+#             else:
+#                 # will sell
+#                 p1 = prices[i] + recur(i+1,True,number+1)
+#                 # not sell
+#                 p2 = recur(i+1,False,number)
+#                 res = max(p1,p2)
             
-            cache[(i,buy,number)] = res
-            return res
+#             cache[(i,buy,number)] = res
+#             return res
         
-        cache = dict()
-        if(len(prices)==0):
+#         cache = dict()
+#         if(len(prices)==0):
+#             return 0
+#         return recur(0,True,0)   
+    
+    
+        # Bottom up
+        n = len(prices)
+        if(n==0):
             return 0
-        return recur(0,True,0)        
+        dp = [[[0 for _ in range(l+1)] for _ in range(2)] for _ in range(n)]
+        
+        for j in range(2):
+            for k in range(l+1):
+                # buy
+                if j==1:
+                    dp[n-1][j][k] = 0
+                # sell
+                else:
+                    dp[n-1][j][k] = prices[n-1]
+                    
+        for i in range(n):
+            for j in range(2):
+                dp[i][j][l] = 0
+                
+        for i in range(n-2,-1,-1):
+            for j in range(2):
+                for k in range(l-1,-1,-1):
+                    if(j==1):
+                        p1 = -prices[i] + dp[i+1][0][k]
+                        p2 = dp[i+1][1][k]
+                        dp[i][j][k] = max(p1,p2)
+                    else:
+                        p1 = prices[i] + dp[i+1][1][k+1]
+                        p2 = dp[i+1][0][k]
+                        dp[i][j][k] = max(p1,p2)
+                        
+        return dp[0][1][0]        
