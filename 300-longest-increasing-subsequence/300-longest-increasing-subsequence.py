73afd147a20ed2bc944acdccb9d4d8ba903f1381
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-08 22:09:40 -0400

    Time: 9952 ms (5.00%), Space: 142.2 MB (5.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,37 +1,38 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        track =[]
-        ans=-1
-        for i in range(len(nums)):
-            prevMax=0
-            for j in range(0,i):
-                if(nums[i]>nums[j]):
-                    prevMax = max(prevMax,track[j])
-                    
-            track.append(prevMax+1)
-            if(track[i]>ans):
-                ans = track[i]
-        return ans
-    
-    
-    # class Solution:
-    # def lengthOfLIS(self, nums: List[int]) -> int:
-    #     track =[]
-    #     ans=-1
-    #     for i in range(len(nums)):
-    #         track.append(1)
-    #         temp=[]
-    #         for j in range(0,i):
-    #             if(nums[i]>nums[j]):
-    #                 temp.append(track[j])
-    #         if(len(temp)>0):
-    #             track[i]=track[i]+max(temp)
-    #         if(track[i]>ans):
-    #             ans = track[i]
-    #     return ans
-    
-    
+#         n = len(nums)
+        
+#         def recur(i,prev):
+#             if(i==n):
+#                 return 0
+#             if(dp[i][prev+1]!=-1 ):
+#                 return dp[i][prev+1]
+#             p1 = 0
+#             if(prev==-1 or nums[prev]<nums[i]):
+#                 p1 = 1 + recur(i+1,i)
+#             p2 = 0 + recur(i+1,prev)
+#             res = max(p1,p2)
+#             dp[i][prev+1] = res
+#             return res
             
+#         dp = [[-1]*(n+1) for _ in range(n)]
+#         return recur(0,-1)
+        
+        n = len(nums)
+        dp = [[0]*(n+1) for _ in range(n+1)]
+        
+                            
+        ans = 0
+        for i in range(n-1,-1,-1):
+            for j in range(i-1,-2,-1):
+                p1 = 0
+                if(j==-1 or nums[j]<nums[i]):
+                    p1 = 1 + dp[i+1][i+1]
+                p2 = dp[i+1][j+1]
+                res = max(p1,p2)
+                dp[i][j+1] = res
+        return dp[0][-1+1]
                 
         
+    
         
