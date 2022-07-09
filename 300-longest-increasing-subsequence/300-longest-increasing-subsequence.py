475a94b044d740f16de3a49fe788093702458580
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-08 22:15:44 -0400

    Time: 9808 ms (5.00%), Space: 14.3 MB (46.68%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -18,20 +18,38 @@ class Solution:
 #         dp = [[-1]*(n+1) for _ in range(n)]
 #         return recur(0,-1)
         
-        n = len(nums)
-        dp = [[0]*(n+1) for _ in range(n+1)]
+        # bottom up
+#         n = len(nums)
+#         dp = [[0]*(n+1) for _ in range(n+1)]
+                     
+#         ans = 0
+#         for i in range(n-1,-1,-1):
+#             for j in range(i-1,-2,-1):
+#                 p1 = 0
+#                 if(j==-1 or nums[j]<nums[i]):
+#                     p1 = 1 + dp[i+1][i+1]
+#                 p2 = dp[i+1][j+1]
+#                 res = max(p1,p2)
+#                 dp[i][j+1] = res
+#         return dp[0][-1+1]
+    
         
-                            
+        # space optimized
+        n = len(nums)
+        curr = [0]*(n+1)
+        nextt = [0]*(n+1)
+                     
         ans = 0
         for i in range(n-1,-1,-1):
             for j in range(i-1,-2,-1):
                 p1 = 0
                 if(j==-1 or nums[j]<nums[i]):
-                    p1 = 1 + dp[i+1][i+1]
-                p2 = dp[i+1][j+1]
+                    p1 = 1 + nextt[i+1]
+                p2 = nextt[j+1]
                 res = max(p1,p2)
-                dp[i][j+1] = res
-        return dp[0][-1+1]
+                curr[j+1] = res
+            nextt = curr.copy()
+        return nextt[-1+1]
                 
         
     
