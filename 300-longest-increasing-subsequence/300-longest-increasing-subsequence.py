f533fde021dc668e0321f6d4f20c4bcd6644e50e
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-08 22:29:53 -0400

    Time: 6866 ms (11.43%), Space: 14.2 MB (46.68%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -35,21 +35,33 @@ class Solution:
     
         
         # space optimized
-        n = len(nums)
-        curr = [0]*(n+1)
-        nextt = [0]*(n+1)
+#         n = len(nums)
+#         curr = [0]*(n+1)
+#         nextt = [0]*(n+1)
                      
-        ans = 0
-        for i in range(n-1,-1,-1):
-            for j in range(i-1,-2,-1):
-                p1 = 0
-                if(j==-1 or nums[j]<nums[i]):
-                    p1 = 1 + nextt[i+1]
-                p2 = nextt[j+1]
-                res = max(p1,p2)
-                curr[j+1] = res
-            nextt = curr.copy()
-        return nextt[-1+1]
+#         ans = 0
+#         for i in range(n-1,-1,-1):
+#             for j in range(i-1,-2,-1):
+#                 p1 = 0
+#                 if(j==-1 or nums[j]<nums[i]):
+#                     p1 = 1 + nextt[i+1]
+#                 p2 = nextt[j+1]
+#                 res = max(p1,p2)
+#                 curr[j+1] = res
+#             nextt = curr.copy()
+#         return nextt[-1+1]
+    
+    
+        # easy understadning dp bottom up solution
+        n = len(nums)
+        dp = [1]*n
+        ans = 1
+        for i in range(1,n):
+            for j in range(i):
+                if(nums[i]>nums[j]):
+                    dp[i] = max(dp[i],dp[j]+1)
+            ans = max(ans, dp[i])
+        return ans
                 
         
     
