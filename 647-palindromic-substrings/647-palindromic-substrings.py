4521cbd558a8c36e569a2532e638a3977a499a91
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-12 23:12:27 -0400

    Time: 179 ms (66.52%), Space: 13.9 MB (77.37%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -40,20 +40,19 @@ class Solution:
             l,r=i,i
             while(l>=0 and r<len(s) and s[l]==s[r]):
                 count = count + 1
-                if(r-l+1>res_len):
-                    res_len = r-l+1
-                    res = s[l:r+1]
+                # if(r-l+1>res_len):
+                #     res_len = r-l+1
+                #     res = s[l:r+1]
                 l=l-1
                 r=r+1
-            
             
             #even length palindromes
             l,r=i,i+1
             while(l>=0 and r<len(s) and s[l]==s[r]):
                 count = count + 1
-                if(r-l+1>res_len):
-                    res_len = r-l+1
-                    res = s[l:r+1]
+                # if(r-l+1>res_len):
+                #     res_len = r-l+1
+                #     res = s[l:r+1]
                 l=l-1
                 r=r+1
         return count
