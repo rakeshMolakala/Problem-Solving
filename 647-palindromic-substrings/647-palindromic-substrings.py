6981147e65cb10b77b4d5fe7e20a05ddaa55984a
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-12 23:12:05 -0400

    Time: 363 ms (33.10%), Space: 14 MB (43.79%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,32 +1,61 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:        
-        n = len(s)
-        res = 0
+#         n = len(s)
+#         res = 0
                     
-        dp=[[False]*n for _ in range(n)]
+#         dp=[[False]*n for _ in range(n)]
         
-        for i in range(n-1,-1,-1):
-            for j in range(i,n):
-                ## for len(s) == 1
-                if(i==j):
-                    dp[i][j] = True
-                    res+=1
+#         for i in range(n-1,-1,-1):
+#             for j in range(i,n):
+#                 ## for len(s) == 1
+#                 if(i==j):
+#                     dp[i][j] = True
+#                     res+=1
                     
-                ## for len(s) == 2
-                elif(j-i==1):
-                    if(s[i]==s[j]):
-                        dp[i][j] = True
-                        res+=1
-                    else:
-                        dp[i][j] = False
+#                 ## for len(s) == 2
+#                 elif(j-i==1):
+#                     if(s[i]==s[j]):
+#                         dp[i][j] = True
+#                         res+=1
+#                     else:
+#                         dp[i][j] = False
                 
-                ## for len(s)>2
-                else:
-                    if(dp[i+1][j-1] and s[i]==s[j]):
-                        dp[i][j] = True
-                        res+=1
-                    else:
-                        dp[i][j] = False
-        return res
+#                 ## for len(s)>2
+#                 else:
+#                     if(dp[i+1][j-1] and s[i]==s[j]):
+#                         dp[i][j] = True
+#                         res+=1
+#                     else:
+#                         dp[i][j] = False
+#         return res
+        
+        #expanding from middle 
+        
+        res =""
+        res_len = 0
+        count = 0
+        
+        for i in range(len(s)):
+            # odd length palindromes
+            l,r=i,i
+            while(l>=0 and r<len(s) and s[l]==s[r]):
+                count = count + 1
+                if(r-l+1>res_len):
+                    res_len = r-l+1
+                    res = s[l:r+1]
+                l=l-1
+                r=r+1
+            
+            
+            #even length palindromes
+            l,r=i,i+1
+            while(l>=0 and r<len(s) and s[l]==s[r]):
+                count = count + 1
+                if(r-l+1>res_len):
+                    res_len = r-l+1
+                    res = s[l:r+1]
+                l=l-1
+                r=r+1
+        return count
                     
                 
