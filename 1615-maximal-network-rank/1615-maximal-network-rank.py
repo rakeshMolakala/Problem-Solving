57bd1e13997ad780c0087b9ad6a3443468659a5c
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-07-15 19:01:38 -0400

    Time: 486 ms (58.17%), Space: 15.6 MB (79.86%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        g = collections.defaultdict(list)
+        for road in roads:
+            g[road[0]].append(road[1])
+            g[road[1]].append(road[0])
+        print(g)
+        
+        maxi = 0
+        for i in range(n):
+            for j in range(i+1,n):
+                if j in g[i]:
+                    maxi = max(maxi,len(g[i])+len(g[j])-1)
+                else:
+                    maxi = max(maxi,len(g[i])+len(g[j]))
+        
+        return maxi
+                
+        
