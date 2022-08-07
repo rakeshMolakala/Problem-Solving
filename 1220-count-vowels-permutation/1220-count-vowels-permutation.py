2ab6d7d8e7e5875d748bd39d8b21043e23d38460
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-07 01:01:02 -0400

    Time: 1339 ms (21.35%), Space: 53.4 MB (37.94%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,8 +1,9 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
         
+        # Top down recursion approach
         temp = ['a','e','i','o','u']
-        dic = {'a':['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['u','i'],'u':['a']}
+        dic = {"":['a','e','i','o','u'],'a':['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['u','i'],'u':['a']}
         cache = dict()
         cons = 1000000007
         
@@ -12,18 +13,19 @@ class Solution:
             if(n==0):
                 return 1
             temp_ans = 0
-            if(prev==""):
-                for c in temp:
-                    temp_ans = temp_ans + recur(c,n-1)
-            else:
-                lis = dic[prev]
-                for c in lis:
-                    temp_ans = temp_ans + recur(c,n-1)
-            temp_ans = temp_ans%cons
+            lis = dic[prev]
+            for c in lis:
+                temp_ans = temp_ans + recur(c,n-1)
+            temp_ans = temp_ans % cons
             cache[(prev,n)] = temp_ans
             return temp_ans
         
         return recur("",n)
+    
+        
+        # Bottom up approach
+        
+        
                 
                 
             
