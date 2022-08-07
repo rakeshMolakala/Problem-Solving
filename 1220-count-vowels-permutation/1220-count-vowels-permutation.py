6ef1a234247c70f1e2444fdbab284c869cac66fa
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-07 02:48:08 -0400

    Time: 245 ms (84.34%), Space: 13.9 MB (93.55%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -2,40 +2,45 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
         
         #Top down recursion approach
-        dic = {"":['a','e','i','o','u'],'a':['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['u','i'],'u':['a']}
-        cache = dict()
-        cons = 1000000007
+#         dic = {"":['a','e','i','o','u'],'a':['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['u','i'],'u':['a']}
+#         cache = dict()
+#         const = 1000000007
         
-        def recur(prev,n):
-            if((prev,n) in cache):
-                return cache[(prev,n)]
-            if(n==0):
-                return 1
-            temp_ans = 0
-            lis = dic[prev]
-            for c in lis:
-                temp_ans = temp_ans + recur(c,n-1)
-            temp_ans = temp_ans % cons
-            cache[(prev,n)] = temp_ans
-            return temp_ans
+#         def recur(prev,n):
+#             if((prev,n) in cache):
+#                 return cache[(prev,n)]
+#             if(n==0):
+#                 return 1
+#             temp_ans = 0
+#             lis = dic[prev]
+#             for c in lis:
+#                 temp_ans = temp_ans + recur(c,n-1)
+#             temp_ans = temp_ans % const
+#             cache[(prev,n)] = temp_ans
+#             return temp_ans
         
-        return recur("",n)
+#         return recur("",n)
     
         
-#         # Bottom up approach
-#         dic = {0:[1,2,3,4,5],1:[2],2:[1,3],3:[1,2,4,5],4:[3,5],5:[1]} 
-#         cons = 1000000007
-#         dp = [[0 for _ in range(n+1)] for _ in range(6)]
-#         for i in range(0,6):
-#             dp[i][0] = 1
-#         for i in range(0,6):
-#             for j in range(1,n+1):
-#                 temp_ans = 0
-#                 for c in dic[i]:
-#                     dp[i][j] = dp[i][j] + dp[c][j-1]
-#                 dp[i][j] = dp[i][j] % cons
-#         print(dp)
-#         return 4
+        # Bottom up approach
+        prev = {'a':1,'e':1,'i':1,'o':1,'u':1,}
+        const = 1000000007
+        # in each iteration for number of extra a's, e's, i's, o's, u's is stored in a map
+        for i in range(2,n+1):
+            a = prev['e'] + prev['i'] + prev['u']
+            e = prev['a'] + prev['i']
+            i = prev['e'] + prev['o']
+            o = prev['i'] 
+            u = prev['i'] + prev['o']
+            
+            prev['a'] = a % const
+            prev['e'] = e % const
+            prev['i'] = i % const
+            prev['o'] = o % const
+            prev['u'] = u % const
+            
+        return sum(list(prev.values())) % const
+        
                 
         
         
