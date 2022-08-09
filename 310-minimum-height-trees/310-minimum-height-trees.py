ffb0ce697fc093e04158ab1827b03a4a97bfd36a
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-08 21:26:28 -0400

    Time: 910 ms (36.10%), Space: 25.2 MB (46.98%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.py
+++ 310-minimum-height-trees/310-minimum-height-trees.py
@@ -6,17 +6,19 @@ class Solution:
                 res.append(i)
             return res
         
+        # creating adjacency list
         adj = [set() for _ in range(n)]
         for e in edges:
             adj[e[0]].add(e[1])
             adj[e[1]].add(e[0])
         
+        # adding first set of leaves to leaves list
         leaves = []
-        # adding all leaves into q
         for i in range(n):
             if(len(adj[i])==1):
                 leaves.append(i)
         
+        # we'll keep on removing nodes that are leaves until we are down to 2 or 1 node
         remain_nodes = n
         while(remain_nodes>2):
             remain_nodes = remain_nodes - len(leaves)
@@ -24,8 +26,11 @@ class Solution:
             
             while leaves:
                 l = leaves.pop()
+                # getting the only neighbor of the lead node
                 neigh = adj[l].pop()
+                # removing the connection 
                 adj[neigh].remove(l)
+                # adding to new_leaves list only if the neighbor is a leaf after removing connection with present leaf
                 if(len(adj[neigh])==1):
                     new_leaves.append(neigh)
             leaves = new_leaves
