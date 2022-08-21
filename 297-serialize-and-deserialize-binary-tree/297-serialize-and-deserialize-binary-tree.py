17e390ef2154d2b4fb7b51527e3c7ace8f41b8cc
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-21 13:34:06 -0400

    Time: 460 ms (11.54%), Space: 20.2 MB (66.03%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -6,38 +6,84 @@
 #         self.right = None
 
 class Codec:
-
+    
     def serialize(self, root):
-        global res
+        if not root:
+            return ""
         res = ""
-        
-        def preorder(root):
-            if not root:
-                global res
-                res = res + ',' + 'n' 
-                return
-            res = res + ',' + str(root.val) 
-            preorder(root.left)
-            preorder(root.right)
+        q = deque()
+        q.append(root)
+        while(q):
+            curr = q.popleft()
+            if(curr==None):
+                res = res + 'None' + ','
+            else:
+                res = res + str(curr.val)+','
+                q.append(curr.left)
+                q.append(curr.right)
+        res = res[0:-1]
+        return res
             
-        preorder(root)
-        return res[1:]
-
     def deserialize(self, data):
-        vals = data.split(',')
-        self.i = 0
+        if(data==""):
+            return None
+        lis = data.split(',')
+        n = len(lis)
+        q = collections.deque()
+        root = TreeNode(lis[0])
+        q.append(root)
+        index = 0
+        while(q):
+            curr = q.popleft()
+            if(index+1<n and lis[index+1]!='None'):
+                curr.left = TreeNode(lis[index+1])
+                q.append(curr.left)
+            if(index+2<n and lis[index+2]!='None'):
+                curr.right = TreeNode(lis[index+2])
+                q.append(curr.right)
+            index=index+2
+        return root
+            
+            
+            
         
-        def dfs():
-            if(vals[self.i]=='n'):
-                self.i+=1
-                return None
-            root = TreeNode(int(vals[self.i]))
-            self.i+=1
-            root.left = dfs()
-            root.right = dfs()
-            return root
         
-        return dfs()
+        
+        
+        
+        
+        
+#     def serialize(self, root):
+#         global res
+#         res = ""
+        
+#         def preorder(root):
+#             if not root:
+#                 global res
+#                 res = res + ',' + 'n' 
+#                 return
+#             res = res + ',' + str(root.val) 
+#             preorder(root.left)
+#             preorder(root.right)
+            
+#         preorder(root)
+#         return res[1:]
+
+#     def deserialize(self, data):
+#         vals = data.split(',')
+#         self.i = 0
+        
+#         def dfs():
+#             if(vals[self.i]=='n'):
+#                 self.i+=1
+#                 return None
+#             root = TreeNode(int(vals[self.i]))
+#             self.i+=1
+#             root.left = dfs()
+#             root.right = dfs()
+#             return root
+        
+#         return dfs()
             
             
         
