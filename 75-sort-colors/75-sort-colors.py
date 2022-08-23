62b04d77579234830dc50a9de5b27af39e7ca9ee
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-23 17:50:41 -0400

    Time: 67 ms (17.25%), Space: 14 MB (15.69%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,8 +4,8 @@ class Solution:
         # dutch flag algorithm
         n = len(nums)
         low = 0
-        high = n-1
         mid = 0
+        high = n-1
         while(mid<=high):
             if(nums[mid]==0):
                 nums[low], nums[mid] = nums[mid], nums[low]
