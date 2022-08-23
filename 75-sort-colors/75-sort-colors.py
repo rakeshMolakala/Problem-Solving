1d2dcf2bea973165c74fbb71be0b5b7bd9163bca
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-23 17:40:15 -0400

    Time: 38 ms (87.18%), Space: 13.9 MB (64.49%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,27 +1,52 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        red = 0
-        white = 0
-        blue = 0
-        for x in nums:
-            if x==0:
-                red+=1
-            elif x==1:
-                white+=1
-            else:
-                blue+=1
-        count = 0
-        for i in range(3):
-            while(red>0):
-                nums[count] = 0
-                count+=1
-                red-=1
-            while(white>0):
-                nums[count] = 1
-                count+=1
-                white-=1
-            while(blue>0):
-                nums[count] = 2
-                count+=1
-                blue-=1
+        #single pass solution 
+        # dutch flag algorithm
+        n = len(nums)
+        low = 0
+        high = n-1
+        mid = 0
+        while(mid<=high):
+            if(nums[mid]==0):
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low+=1
+                mid+=1
+            elif(nums[mid]==1):
+                mid+=1
+            elif(nums[mid]==2):
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high-=1
+
+        
+        
+        
+        
+        
+        
+        
+#         # brute force and used two passes
+#         red = 0
+#         white = 0
+#         blue = 0
+#         for x in nums:
+#             if x==0:
+#                 red+=1
+#             elif x==1:
+#                 white+=1
+#             else:
+#                 blue+=1
+#         count = 0
+#         for i in range(3):
+#             while(red>0):
+#                 nums[count] = 0
+#                 count+=1
+#                 red-=1
+#             while(white>0):
+#                 nums[count] = 1
+#                 count+=1
+#                 white-=1
+#             while(blue>0):
+#                 nums[count] = 2
+#                 count+=1
+#                 blue-=1
             
