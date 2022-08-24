aaeeca58ce6ed3a8ae86c093deb3f7548f5e285e
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-23 20:11:43 -0400

    Time: 1133 ms (60.09%), Space: 39 MB (76.71%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,11 +5,34 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        l = []
-        while(head):
-            l.append(head.val)
-            head = head.next
-        return l==l[::-1]
+        # finding middle node
+        slow = head
+        fast = head
+        while(fast and fast.next):
+            fast = fast.next.next
+            slow = slow.next
+            
+        # now slow is at the middle node
+        # now we reverse the linked list from middle to last
+        
+        prev = None
+        while(slow):
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        # now as the second half is reversed, we use two pointers technique and come from front and back and check the values are same or not
+        
+        left = head
+        right = prev
+        while(right):
+            if right.val!=left.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+        
         
         
         
