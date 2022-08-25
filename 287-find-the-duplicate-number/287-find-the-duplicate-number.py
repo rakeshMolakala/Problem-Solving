69801ee01a4b605e9ba4b1fae8e6ba3a0cbd01ad
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-24 23:35:37 -0400

    Time: 650 ms (95.45%), Space: 27.9 MB (91.47%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # assigning slow and fast pointers to starting number of LL
         slow, fast = nums[0],nums[0]
         while True:
             slow = nums[slow]
@@ -7,10 +8,14 @@ class Solution:
             if slow==fast:
                 break
                 
+        # Setting a new pointer from start of the LL and finding the start node of cycle in the LL
         start_slow = nums[0]
         while True:
             if(slow==start_slow):
                 return slow
             slow = nums[slow]
             start_slow = nums[start_slow]
+            
+            
+            
             
