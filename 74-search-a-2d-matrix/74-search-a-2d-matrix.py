8c9049320b49a5495a5b862d4065f6eeaaedae6b
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-27 17:58:01 -0400

    Time: 75 ms (40.47%), Space: 14.4 MB (43.33%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,21 +1,46 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for x in matrix:
-            if(self.binarySearch(x,target)):
-                return True
-        return False
+        rows = len(matrix)
+        cols = len(matrix[0])
+        low = 0
+        high = rows*cols - 1
         
-    def binarySearch(self,arr,target):
-        low=0
-        high=len(arr)-1
+        #doing binary search
         while(low<=high):
-            mid=(low+high)//2
-            if(arr[mid]<target):
-                low=mid+1
-            elif(arr[mid]>target):
-                high=mid-1
+            mid = (low+high)//2
+            i = mid // cols
+            j = mid % cols
+            curr = matrix[i][j]
+            if(target<curr):
+                high = mid-1
+            elif(target>curr):
+                low = mid+1
             else:
                 return True
         return False
+    
+    
+    
+    
+    
+    
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         for x in matrix:
+#             if(self.binarySearch(x,target)):
+#                 return True
+#         return False
+        
+#     def binarySearch(self,arr,target):
+#         low=0
+#         high=len(arr)-1
+#         while(low<=high):
+#             mid=(low+high)//2
+#             if(arr[mid]<target):
+#                 low=mid+1
+#             elif(arr[mid]>target):
+#                 high=mid-1
+#             else:
+#                 return True
+#         return False
                 
         
