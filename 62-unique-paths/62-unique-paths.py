d15e2d690b6a9aca68a95d6021bbd6bac0a58fa6
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-28 11:42:24 -0400

    Time: 46 ms (60.18%), Space: 14.3 MB (6.47%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,24 +1,22 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        cache = dict()
         
-        
-        def recur(row,col):
-            if((row,col) in cache):
-                return cache[(row,col)]
-            
-            if(row>=m or col>=n):
+        def recur(i,j):
+            if i>=m or j>=n:
                 return 0
-            if(row==m-1 and col==n-1):
+            if i==m-1 and j==n-1:
                 return 1
-            
-            p1 = recur(row+1,col)
-            p2 = recur(row,col+1)
-            res = p1+p2
-            cache[(row,col)] = res
+            if (i,j) in cache:
+                return cache[(i,j)]
+            down = recur(i+1,j)
+            right = recur(i,j+1)
+            res = down + right
+            cache[(i,j)] = res
             return res
         
-        cache = dict()
         return recur(0,0)
+            
         
         
         
@@ -50,6 +48,37 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        
+        
+        # Top down approach
+#         def recur(row,col):
+#             if((row,col) in cache):
+#                 return cache[(row,col)]
+            
+#             if(row>=m or col>=n):
+#                 return 0
+#             if(row==m-1 and col==n-1):
+#                 return 1
+            
+#             p1 = recur(row+1,col)
+#             p2 = recur(row,col+1)
+#             res = p1+p2
+#             cache[(row,col)] = res
+#             return res
+        
+#         cache = dict()
+#         return recur(0,0)
+        
+        
+        
+        
+        
+#         Bottom up approach
+
 #         dp=[[0]*n for i in range(m)]
 #         dp[0][0] = 1
 #         for i in range(1,m):
