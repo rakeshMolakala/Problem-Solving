ca9675000f7b2abd97251c09e30b8b7094b60b6a
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-29 20:07:32 -0400

    Time: 2204 ms (31.94%), Space: 28.9 MB (59.84%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,27 +4,16 @@ class Solution:
         n = len(nums)
         for i in range(n):
             track.add(nums[i])
-        visited = set()
              
         res = 0
         for i in range(n):
-            if nums[i] in visited:
-                continue
-            left = nums[i]-1
-            right = nums[i]+1
-            left_count = 0
-            right_count = 0
-            while(left in track):
-                left_count+=1
-                left = left - 1
-                visited.add(left)
-            
-            while(right in track):
-                right_count+=1
-                right = right + 1
-                visited.add(right)
-            
-            res = max(res,1+left_count+right_count)
+            if nums[i]-1 not in track:
+                right = nums[i] + 1
+                right_count = 0
+                while(right in track):
+                    right_count+=1
+                    right = right + 1
+                res = max(res,1+right_count)
         return res
             
             
