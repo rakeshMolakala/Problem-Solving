a2ed52fb0ad5fee5cdcdd0ad27c1f058ddb87dd2
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-08-31 04:50:44 -0400

    Time: 176 ms (42.45%), Space: 19.7 MB (5.96%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,23 +1,73 @@
 import heapq
 class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+#         # first approach using heaps
+#         track = collections.defaultdict(int)
+#         for num in nums:
+#             track[num]+=1
+        
+#         heap = []
+#         for key,value in track.items():
+#             heap.append((-value,key))
+#         heapq.heapify(heap)
+        
+#         res = []
+#         for i in range(k):
+#             value,key = heapq.heappop(heap)
+#             res.append(key)
+#         return res
+            
+        
+    # second approach using bucket sort - O(n)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         # first approach using heaps
         track = collections.defaultdict(int)
         for num in nums:
             track[num]+=1
-        
-        heap = []
-        for key,value in track.items():
-            heap.append((-value,key))
-        heapq.heapify(heap)
+            
+        n = len(nums)
+        temp = [[] for _ in range(n+1)]
+        for key,val in track.items():
+            temp[val].append(key)
         
         res = []
-        for i in range(k):
-            value,key = heapq.heappop(heap)
-            res.append(key)
+        for i in range(n,0,-1):
+            for ele in temp[i]:
+                if k==0:
+                    return res
+                res.append(ele)
+                k-=1
         return res
             
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
        
         # Second approach usign Bucket sort technique - O(n) solution
 #         if(k==len(nums)):
