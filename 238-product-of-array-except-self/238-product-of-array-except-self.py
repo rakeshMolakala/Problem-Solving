384c29347f21539d6d01d13686ed9207be926ad5
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-09-08 19:28:34 -0400

    Time: 663 ms (5.01%), Space: 21.3 MB (56.91%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,23 +1,45 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        prefix = [1]*n
-        suffix = [1]*n
-        prefix[0] = nums[0]
-        suffix[-1] = nums[-1]
+        res = [1]*n
         
         for i in range(1,n):
-            prefix[i] = nums[i]*prefix[i-1]
+            res[i] = res[i-1]*nums[i-1]
             
-        for i in range(n-2,-1,-1):
-            suffix[i] = nums[i]*suffix[i+1]
-            
-            
-        for i in range(n):
-            nums[i] = 1
-            if i-1>=0:
-                nums[i] = nums[i]*prefix[i-1]
-            if i+1<n:
-                nums[i] = nums[i]*suffix[i+1]
+        right_product = 1
+        for i in range(n-1,-1,-1):
+            res[i] = res[i]*right_product
+            right_product = right_product*nums[i]
         
-        return nums
+        return res
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+          # TC - O(n) and SC - O(n)
+#         n = len(nums)
+#         prefix = [1]*n
+#         suffix = [1]*n
+#         prefix[0] = nums[0]
+#         suffix[-1] = nums[-1]
+        
+#         for i in range(1,n):
+#             prefix[i] = nums[i]*prefix[i-1]
+            
+#         for i in range(n-2,-1,-1):
+#             suffix[i] = nums[i]*suffix[i+1]
+            
+#         for i in range(n):
+#             nums[i] = 1
+#             if i-1>=0:
+#                 nums[i] = nums[i]*prefix[i-1]
+#             if i+1<n:
+#                 nums[i] = nums[i]*suffix[i+1]
+        
+#         return nums
