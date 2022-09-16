9516111780cec34690da0f336bd8b8ad94302228
Rakesh Molakala <91920989+rakeshMolakala@users.noreply.github.com>
2022-09-16 18:08:33 -0400

    Time: 3746 ms (17.02%), Space: 32.7 MB (46.69%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -3,35 +3,67 @@ class Solution:
         n = len(changed)
         if n%2!=0:
             return []
-        
-        res =[]
-        
         track = collections.defaultdict(int)
         for num in changed:
             track[num]+=1
-            
-        for num in sorted(track.keys()):
+        
+        res = []
+        changed.sort()
+        
+        for num in changed:
             if track[num]>0:
+                track[num]-=1
                 
-                if num==0:
-                    count = track[num]
-                    if count%2!=0:
-                        return []
-                    res = res + [0]*(count//2)
-                    track[num] = track[num] - count
-                
-                elif 2*num in track:
-                    count = track[num]
-                    double_count = track[2*num]
-                    if double_count<count:
-                        return []
-                    res = res + [num]*(count)
-                    track[num] = track[num] - count
-                    track[2*num] = track[2*num] - count
-                    
+                if 2*num in track:
+                    track[2*num]-=1
+                    res.append(num)
                 else:
                     return []
         
         if len(res)==n//2:
             return res
         return []
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(changed)
+#         if n%2!=0:
+#             return []
+        
+#         res =[]
+        
+#         track = collections.defaultdict(int)
+#         for num in changed:
+#             track[num]+=1
+            
+#         for num in sorted(track.keys()):
+#             if track[num]>0:
+                
+#                 if num==0:
+#                     count = track[num]
+#                     if count%2!=0:
+#                         return []
+#                     res = res + [0]*(count//2)
+#                     track[num] = track[num] - count
+                
+#                 elif 2*num in track:
+#                     count = track[num]
+#                     double_count = track[2*num]
+#                     if double_count<count:
+#                         return []
+#                     res = res + [num]*(count)
+#                     track[num] = track[num] - count
+#                     track[2*num] = track[2*num] - count
+                    
+#                 else:
+#                     return []
+        
+#         if len(res)==n//2:
+#             return res
+#         return []
